d05b2ce9f67a91019c612d600504388304660266
pcw109550 <pcw109550@gmail.com>
2023-03-23 19:14:50 +0900

    Recover log

--- turbo/app/recover.go
+++ turbo/app/recover.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -14,6 +15,8 @@ import (
 	"github.com/ledgerwatch/erigon/core/rawdb"
 	"github.com/ledgerwatch/erigon/core/types"
 	"github.com/ledgerwatch/erigon/eth"
+	"github.com/ledgerwatch/erigon/eth/stagedsync"
+	"github.com/ledgerwatch/erigon/ethdb/prune"
 	turboNode "github.com/ledgerwatch/erigon/turbo/node"
 	"github.com/ledgerwatch/log/v3"
 	"github.com/urfave/cli/v2"
@@ -40,6 +43,20 @@ var recoverSendersCommand = cli.Command{
 	Category: "BLOCKCHAIN COMMANDS",
 	Description: `
 The recover command recovers Senders table.`,
+}
+
+var recoverLogCommand = cli.Command{
+	Action:    MigrateFlags(recoverLog),
+	Name:      "recover-log",
+	Usage:     "Recover log",
+	ArgsUsage: "<blockNumFirst> <blockNumLast>",
+	Flags: []cli.Flag{
+		&utils.DataDirFlag,
+		&utils.ChainFlag,
+	},
+	Category: "BLOCKCHAIN COMMANDS",
+	Description: `
+The recover command recovers LogTopicIndex table and LogAddressIndex table.`,
 }
 
 func recoverSenders(ctx *cli.Context) error {
@@ -175,6 +192,137 @@ func RecoverSendersBatch(ethereum *eth.Ethereum, signer *types.Signer, start, en
 		if err := rawdb.WriteSenders(tx, block.Hash(), nr, sendersRecovered); err != nil {
 			return err
 		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func recoverLog(ctx *cli.Context) error {
+	if ctx.NArg() != 2 {
+		utils.Fatalf("This command requires an argument.")
+	}
+
+	first, ferr := strconv.ParseInt(ctx.Args().Get(0), 10, 64)
+	last, lerr := strconv.ParseInt(ctx.Args().Get(1), 10, 64)
+	if ferr != nil || lerr != nil {
+		utils.Fatalf("Recover error in parsing parameters: block number not an integer\n")
+	}
+	if first < 0 || last < 0 {
+		utils.Fatalf("Recover error: block number must be greater than 0\n")
+	}
+
+	logger := log.New(ctx)
+
+	nodeCfg := turboNode.NewNodConfigUrfave(ctx)
+	ethCfg := turboNode.NewEthConfigUrfave(ctx, nodeCfg)
+
+	stack := makeConfigNode(nodeCfg)
+	defer stack.Close()
+
+	ethereum, err := eth.New(stack, ethCfg, logger)
+	if err != nil {
+		return err
+	}
+	err = ethereum.Init(stack, ethCfg)
+	if err != nil {
+		return err
+	}
+
+	if err := RecoverLog(ethereum, uint64(first), uint64(last)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func RecoverLog(ethereum *eth.Ethereum, first, last uint64) error {
+	// Watch for Ctrl-C while the import is running.
+	// If a signal is received, the import will stop at the next batch.
+	interrupt := make(chan os.Signal, 1)
+	stop := make(chan struct{})
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(interrupt)
+	defer close(interrupt)
+	go func() {
+		if _, ok := <-interrupt; ok {
+			log.Info("Interrupted during recovery, stopping at next batch")
+		}
+		close(stop)
+	}()
+	checkInterrupt := func() bool {
+		select {
+		case <-stop:
+			return true
+		default:
+			return false
+		}
+	}
+
+	log.Info("Recovering Log")
+	n := first
+	startTime, reportedTime := time.Now(), time.Now()
+	for batch := 0; ; batch++ {
+		if checkInterrupt() {
+			return fmt.Errorf("interrupted")
+		}
+		start := n
+		end := n + recoverBatchSize - 1
+		if end > last {
+			end = last
+		}
+		if err := RecoverLogBatch(ethereum, start, end); err != nil {
+			return err
+		}
+		if end == last {
+			break
+		}
+		n += recoverBatchSize
+
+		if time.Since(reportedTime) >= 8*time.Second {
+			log.Info("Recovering Log", "recovered", start, "elapsed", time.Duration(time.Since(startTime)))
+			reportedTime = time.Now()
+		}
+	}
+	return nil
+}
+
+func RecoverLogBatch(ethereum *eth.Ethereum, start, end uint64) error {
+	db := ethereum.ChainDB()
+	tx, err := db.BeginRw(ethereum.SentryCtx())
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	// receipt sanity check
+	// for nr := start; nr <= end; nr++ {
+	// 	block, err := rawdb.ReadBlockByNumber(tx, nr)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	receipts := rawdb.ReadRawReceipts(tx, nr)
+
+	// 	receiptHash := types.DeriveSha(receipts)
+	// 	if receiptHash != block.ReceiptHash() {
+	// 		log.Error("receipt root mismatch", "blockNum", nr, "receiptHash", receiptHash.Hex(), "newReceiptHash", block.ReceiptHash().Hex())
+	// 		return errors.New("receipt trie root mismatch. aborting")
+	// 	}
+	// }
+
+	return nil
+
+	pm := prune.DefaultMode
+	dirs := ethereum.Dirs()
+	logPrefix := ""
+	ctx := context.Background()
+
+	cfg := stagedsync.StageLogIndexCfg(db, pm, dirs.Tmp)
+	if err = stagedsync.PromoteLogIndex(logPrefix, tx, start, end, cfg, ctx); err != nil {
+		return err
 	}
 
 	if err := tx.Commit(); err != nil {
