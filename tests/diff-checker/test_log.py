298ba8ced73e78bb547a614274719a0d315120ce
pcw109550 <pcw109550@gmail.com>
2023-03-28 13:13:27 +0900

    Add tests for logs for overlap
    Check that quering from prebedrock to bedrock works

--- tests/diff-checker/test_log.py
+++ tests/diff-checker/test_log.py
@@ -3,8 +3,7 @@ import random
 import pytest
 import web3
 
-from common import (RPCMethod, compare_txs, erigon, erigon_client, geth,
-                    geth_client)
+from common import RPCMethod, compare_txs, erigon, erigon_client, geth, geth_client
 from constants import BEDROCK_START
 
 transfer_topic = web3.Web3.keccak(text="Transfer(address,address,uint256)").hex()
@@ -42,6 +41,41 @@ def test_eth_getLogs(bedrock):
                     "topics": topics,
                     "fromBlock": hex(target_block_number),
                     "toBlock": hex(target_block_number + 100),
+                },
+            ],
+        )
+        assert geth_res == erigon_res
+
+
+target_topic = web3.Web3.keccak(
+    text="SentMessage(address,address,bytes,uint256,uint256)"
+).hex()
+# restricting target topic by 1 to avoid timeout for geth client
+target_topics = [target_topic]
+
+
+def test_eth_getLogs_overlap():
+    max_block_number = min(erigon.eth.block_number, geth.eth.block_number)
+    for _ in range(16):
+        start_block_number = random.randint(0, BEDROCK_START - 101)
+        end_block_number = random.randint(BEDROCK_START, max_block_number - 100)
+        geth_res = geth_client.send_request(
+            RPCMethod.GetLogs,
+            params=[
+                {
+                    "topics": target_topics,
+                    "fromBlock": hex(start_block_number),
+                    "toBlock": hex(end_block_number + 100),
+                },
+            ],
+        )
+        erigon_res = erigon_client.send_request(
+            RPCMethod.GetLogs,
+            params=[
+                {
+                    "topics": target_topics,
+                    "fromBlock": hex(start_block_number),
+                    "toBlock": hex(end_block_number + 100),
                 },
             ],
         )
