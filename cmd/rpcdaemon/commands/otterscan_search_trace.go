b13c4dd3b60013cf2b0ee217147081f2a9ac8332
Tei Im <tei.im@testinprod.io>
2023-05-02 13:30:34 +0900

    Fix type mismatch

--- cmd/rpcdaemon/commands/otterscan_search_trace.go
+++ cmd/rpcdaemon/commands/otterscan_search_trace.go
@@ -91,7 +91,7 @@ func (api *OtterscanAPIImpl) traceBlock(dbtx kv.Tx, ctx context.Context, blockNu
 	}
 	engine := api.engine()
 
-	blockReceipts := rawdb.ReadReceipts(api._chainConfig, dbtx, block, senders)
+	blockReceipts := rawdb.ReadReceipts(api._chainConfig.Load(), dbtx, block, senders)
 	header := block.Header()
 	rules := chainConfig.Rules(block.NumberU64(), header.Time)
 	found := false
