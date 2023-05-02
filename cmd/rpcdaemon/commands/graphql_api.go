fc1e3303a4795e606c6cc0d7aaa78c3d486ca2a5
Tei Im <tei.im@testinprod.io>
2023-05-02 13:31:08 +0900

    Get depositNonces in GraphQLAPI

--- cmd/rpcdaemon/commands/graphql_api.go
+++ cmd/rpcdaemon/commands/graphql_api.go
@@ -109,7 +109,8 @@ func (api *GraphQLAPIImpl) delegateGetBlockByNumber(tx kv.Tx, b *types.Block, nu
 		return nil, err
 	}
 	additionalFields := make(map[string]interface{})
-	response, err := ethapi.RPCMarshalBlock(b, inclTx, inclTx, additionalFields)
+	depositNonces := rawdb.ReadDepositNonces(tx, uint64(number.Int64()))
+	response, err := ethapi.RPCMarshalBlock(b, inclTx, inclTx, additionalFields, depositNonces)
 	if !inclTx {
 		delete(response, "transactions") // workaround for https://github.com/ledgerwatch/erigon/issues/4989#issuecomment-1218415666
 	}
