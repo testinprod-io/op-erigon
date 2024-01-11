66ad417f7421d664f2a90ae8a913367747a81246
Tei Im <tei.im@testinprod.io>
2024-01-12 04:15:48 +0900

    Update version.go

--- params/version.go
+++ params/version.go
@@ -44,7 +44,7 @@ const (
 const (
 	OPVersionMajor    = 0          // Major version component of the current release
 	OPVersionMinor    = 2          // Minor version component of the current release
-	OPVersionMicro    = 0          // Patch version component of the current release
+	OPVersionMicro    = 1          // Patch version component of the current release
 	OPVersionModifier = "unstable" // Version metadata to append to the version string
 )
 
