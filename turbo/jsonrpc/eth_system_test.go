4e2bf5d50422fd9667019a2a481132dd9313d170
Minhyuk Kim <kimminhyuk1004@gmail.com>
2024-06-27 01:50:08 +0900

    Update erigon-lib/go.sum and fix tests

--- turbo/jsonrpc/eth_system_test.go
+++ turbo/jsonrpc/eth_system_test.go
@@ -11,8 +11,6 @@ import (
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/log/v3"
 
-	"github.com/ledgerwatch/log/v3"
-
 	"github.com/ledgerwatch/erigon/core"
 	"github.com/ledgerwatch/erigon/core/types"
 	"github.com/ledgerwatch/erigon/crypto"
