74f22b3204ad5c34eab21148aef3ec653a91e5a7
Minhyuk Kim <kimminhyuk1004@gmail.com>
2024-09-13 13:53:31 +0900

    Fix build error

--- cmd/rpcdaemon/main.go
+++ cmd/rpcdaemon/main.go
@@ -76,7 +76,7 @@ func main() {
 			historicalRPCService = client
 		}
 
-		apiList := jsonrpc.APIList(db, backend, txPool, mining, ff, stateCache, blockReader, cfg, engine, nil, nil logger, nil)
+		apiList := jsonrpc.APIList(db, backend, txPool, mining, ff, stateCache, blockReader, cfg, engine, seqRPCService, historicalRPCService, logger, nil)
 		rpc.PreAllocateRPCMetricLabels(apiList)
 		if err := cli.StartRpcServer(ctx, cfg, apiList, logger); err != nil {
 			logger.Error(err.Error())
