cd2a1e275dd40de5d8e89b380a1ba4596230d2ef
Tei Im <tei.im@testinprod.io>
2025-04-08 16:46:11 +0900

    Remove requests queue processing

--- consensus/merge/merge.go
+++ consensus/merge/merge.go
@@ -161,7 +161,7 @@ func (s *Merge) Finalize(config *chain.Config, header *types.Header, state *stat
 	}
 
 	var rs types.FlatRequests
-	if config.IsPrague(header.Time) {
+	if config.IsPrague(header.Time) && !config.IsIsthmus(header.Time) {
 		rs = make(types.FlatRequests, 0)
 		allLogs := make(types.Logs, 0)
 		for _, rec := range receipts {
